d1e03008e3ec0c45292a1be3f712f73d72b13d82
PiotrCiborowski <pcibor@gazeta.pl>
2020-01-18 10:56:56 +0100

    first

---
+++ ClassLibrary/DiceManagement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    class DiceManagement
+    {
+        Random rnd = new Random();
+
+        public int ThrowDice()
+        {
+            return rnd.Next(1, 7);
+        }
+    }
+}
