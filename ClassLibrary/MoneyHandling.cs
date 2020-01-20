a66184b36c9a01f7a1a92b5fffd3a1246cce85a9
PiotrCiborowski <pcibor@gazeta.pl>
2020-01-20 19:00:46 +0100

    Game Mechanics

---
+++ ClassLibrary/MoneyHandling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class MoneyHandling
+    {
+        public static int CheckBetMoney(int betMoney, int money)
+        {
+            if (betMoney > 0 && betMoney <= money)
+                return 1;
+            else if (betMoney > money)
+                return -1;
+            else
+                return 0;
+        }
+
+        public static int MakeMoney(int betMoney, int money, int operation)
+        {
+            if (operation == 1)
+                return money += betMoney;
+            else if (operation == -1)
+                return money -= betMoney;
+            else
+                return money;
+        }
+    }
+}
