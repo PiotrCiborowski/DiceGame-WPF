788468bb76f8a0a7684c02ddae1f973d0bc62ee6
PiotrCiborowski <pcibor@gazeta.pl>
2020-01-22 23:05:33 +0100

    Final version of application logic

--- ProjektDiceGame/MainWindow.xaml.cs
+++ ProjektDiceGame/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -17,22 +19,80 @@ using ClassLibrary;
 
 namespace ProjektDiceGame
 {
+    public class ResultType
+    {
+        public string Name { get; set; }
+
+        public string Result { get; set; }
+    }
+
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         int enemyD1, enemyD2, myD1, myD2, betMoney, money;
+        static string resultPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        ResourceManager rm = new ResourceManager(typeof(Properties.Resources));
+        List<ResultType> resultArray = new List<ResultType>();
 
         public MainWindow()
         {
             InitializeComponent();
 
+            StartGame();
+        }
+
+        private void StartGame()
+        {
             Money.Content = "100";
             BetMoney.Text = "";
 
             SetupEnemies();
+
+            LoadResults();
         }
+
+        static async Task SaveResults(string name, string result)
+        {
+            string line = name + "-" + result;
+
+            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(resultPath, "Results.txt"), true))
+            {
+                await outputFile.WriteLineAsync(line);
+            }
+        }
+
+        private void LoadResults()
+        {
+            resultArray.Clear();
+            ResultSpace.ItemsSource = null;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(System.IO.Path.Combine(resultPath, "Results.txt")))
+                {
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        int index = line.IndexOf('-');
+                        string name = line.Substring(0, index);
+                        string result = line.Substring(index + 1);
+
+                        resultArray.Add(new ResultType() { Name = name, Result = result });
+                    }
+                }
+
+                ResultSpace.ItemsSource = resultArray;
+            }
+
+            catch (Exception e)
+            {
+                //MessageBox.Show("The file could not be loaded: " + e);
+            }
+        }
+
 
         private void SetupEnemies()
         {
@@ -54,13 +114,58 @@ namespace ProjektDiceGame
 
         private void ResetGame()
         {
+            EnemyDie1.Source = new BitmapImage(new Uri("images/D0.png", UriKind.Relative));
+            EnemyDie2.Source = new BitmapImage(new Uri("images/D0.png", UriKind.Relative));
             MyDie1.Source = new BitmapImage(new Uri("images/D0.png", UriKind.Relative));
             MyDie2.Source = new BitmapImage(new Uri("images/D0.png", UriKind.Relative));
 
             BetMoney.Text = "";
 
-            SetupEnemies();
+            Int32.TryParse(Money.Content.ToString(), out money);
+
+            if (money <= 0)
+            {
+                MessageBox.Show(string.Format(rm.GetString("LossGame")));
+
+                NameTyper dialog = new NameTyper();
+                //dialog.Show();
+
+                if (dialog.ShowDialog() == true)
+                {
+                    string name = dialog.NameText;
+                    Task.Run(() => SaveResults(name, "Lost")).Wait();
+                    StartGame();
+                }
+            }
+            else if (money >= 1000)
+            {
+                MessageBox.Show(string.Format(rm.GetString("WinGame")));
+
+                NameTyper dialog = new NameTyper();
+                //dialog.Show();
+
+                if (dialog.ShowDialog() == true)
+                {
+                    string name = dialog.NameText;
+                    Task.Run(() => SaveResults(name, "Win")).Wait();
+                    StartGame();
+                }
+            }
+            else
+                SetupEnemies();
         }
+
+        /*private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+
+            Window.UpdateLayout();
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("pl-PL");
+        }*/
 
         private void Higher_Click(object sender, RoutedEventArgs e)
         {
@@ -72,26 +177,31 @@ namespace ProjektDiceGame
                 SetupMine();
                 if (DiceManagement.CheckDiceHigher(enemyD1, enemyD2, myD1, myD2) == -1)
                 {
-                    MessageBox.Show("You lost!");
+                    //MessageBox.Show("You lost!");
+                    MessageBox.Show(string.Format(rm.GetString("Loss")));
                     Money.Content = MoneyHandling.MakeMoney(betMoney, money, -1).ToString();
                 }
                 else if (DiceManagement.CheckDiceHigher(enemyD1, enemyD2, myD1, myD2) == 1)
                 {
-                    MessageBox.Show("You won!");
+                    //MessageBox.Show("You won!");
+                    MessageBox.Show(string.Format(rm.GetString("Win")));
                     Money.Content = MoneyHandling.MakeMoney(betMoney, money, 1).ToString();
                 }
                 else if (DiceManagement.CheckDiceHigher(enemyD1, enemyD2, myD1, myD2) == 0)
                 {
-                    MessageBox.Show("You tied!");
+                    //MessageBox.Show("You tied!");
+                    MessageBox.Show(string.Format(rm.GetString("Tie")));
                 }
 
                 ResetGame();
             }
             else if (MoneyHandling.CheckBetMoney(betMoney, money) == 0)
-                MessageBox.Show("Set amount of money you want to bet!");
+                //MessageBox.Show("Set amount of money you want to bet!");
+                MessageBox.Show(string.Format(rm.GetString("NoMoney")));
             else if (MoneyHandling.CheckBetMoney(betMoney, money) == -1)
             {
-                MessageBox.Show("You can't bet more money than you have!");
+                //MessageBox.Show("You can't bet more money than you have!");
+                MessageBox.Show(string.Format(rm.GetString("OverMoney")));
                 BetMoney.Text = money.ToString();
             }
         }
@@ -106,26 +216,31 @@ namespace ProjektDiceGame
                 SetupMine();
                 if (DiceManagement.CheckDiceLower(enemyD1, enemyD2, myD1, myD2) == -1)
                 {
-                    MessageBox.Show("You lost!");
+                    //MessageBox.Show("You lost!");
+                    MessageBox.Show(string.Format(rm.GetString("Loss")));
                     Money.Content = MoneyHandling.MakeMoney(betMoney, money, -1).ToString();
                 }
                 else if (DiceManagement.CheckDiceLower(enemyD1, enemyD2, myD1, myD2) == 1)
                 {
-                    MessageBox.Show("You won!");
+                    //MessageBox.Show("You won!");
+                    MessageBox.Show(string.Format(rm.GetString("Win")));
                     Money.Content = MoneyHandling.MakeMoney(betMoney, money, 1).ToString();
                 }
                 else if (DiceManagement.CheckDiceLower(enemyD1, enemyD2, myD1, myD2) == 0)
                 {
-                    MessageBox.Show("You tied!");
+                    //MessageBox.Show("You tied!");
+                    MessageBox.Show(string.Format(rm.GetString("Tie")));
                 }
 
                 ResetGame();
             }
             else if (MoneyHandling.CheckBetMoney(betMoney, money) == 0)
-                MessageBox.Show("Set amount of money you want to bet!");
+                //MessageBox.Show("Set amount of money you want to bet!");
+                MessageBox.Show(string.Format(rm.GetString("NoMoney")));
             else if (MoneyHandling.CheckBetMoney(betMoney, money) == -1)
             {
-                MessageBox.Show("You can't bet more money than you have!");
+                //MessageBox.Show("You can't bet more money than you have!");
+                MessageBox.Show(string.Format(rm.GetString("OverMoney")));
                 BetMoney.Text = money.ToString();
             }
         }
---
+++ ProjektDiceGame/NameTyper.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjektDiceGame
+{
+    /// <summary>
+    /// Logika interakcji dla klasy NameTyper.xaml
+    /// </summary>
+    public partial class NameTyper : Window
+    {
+        public NameTyper()
+        {
+            InitializeComponent();
+
+            NameBox.Text = "";
+        }
+
+        public string NameText
+        { 
+            get { return NameBox.Text; }
+            set { NameBox.Text = value; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                Button_Click(sender, e);
+        }
+    }
+}
